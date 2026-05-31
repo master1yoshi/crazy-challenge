1a934f038e483ee5ace8b7389b49e44082ff4acb
master1yoshi <achrafmalki448@gmail.com>
2026-05-31 20:51:57 +0100

    fix: port parsing and ssl config

--- server.js
+++ server.js
@@ -18,12 +18,14 @@ app.use('/socket.io', express.static(path.join(__dirname, 'node_modules/socket.i
 
 // 1. CONNEXION BASE DE DONNÉES SÉCURISÉE (Correction de l'adresse Host ajoutée ici !)
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'mysql-crazychallenge-achrafmalki448-c95d.l.aivencloud.com',
-    port: process.env.DB_PORT || 13187,
-    user: process.env.DB_USER || 'avnadmin',
-    password: process.env.DB_PASSWORD || 'AVNS_urj7HOP6C8wvfRmHmIa', 
-    database: process.env.DB_DATABASE || 'defaultdb',
-    ssl: { rejectUnauthorized: false }
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 13187,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
